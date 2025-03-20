d243f1761dcf8ffdd4521929ea605253e5c9da77
irem0ckmk <cakmakirem@posta.mu.edu.tr>
2025-03-20 13:15:40 +0300

    Week 5 completed

---
+++ week5/src/Circle.java
@@ -0,0 +1,30 @@
+public class Circle {
+    private Point center;
+    private int radius;
+
+    public Circle(Point center,int radius,) {
+        this.radius = radius;
+        this.center = center;
+    }
+
+    public Point getCenter() {
+        return center;
+    }
+
+    public void setCenter(Point center) {
+        this.center = center;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        if (radius < 0){
+            this.radius = 0;
+            System.out.println("Radius can't be negative.");
+        }else {
+            this.radius = radius;
+        }
+    }
+}
---
+++ week5/src/Rectangle.java
@@ -0,0 +1,64 @@
+public class Rectangle {
+    private Point topLeft;
+    private int sideA, sideB;
+
+    public Rectangle(Point topLeft, int sideA, int sideB) {
+        this.topLeft = topLeft;
+        this.sideA = sideA;
+        this.sideB = sideB;
+        setSideA(sideA);
+        setSideB(sideB);
+    }
+
+    public Rectangle(Point topLeft, int sideA) {
+        this.topLeft = topLeft;
+        setSideA(sideA);
+        setSideB(sideA);
+        this.sideA = sideA;
+        this.sideB = sideA;
+
+    }
+
+    public Point getTopLeft() {
+        return topLeft;
+    }
+
+    public void setTopLeft(Point topLeft) {
+        this.topLeft = topLeft;
+    }
+
+    public int getSideA() {
+        return sideA;
+    }
+
+    public void setSideA(int sideA) {
+        if(sideA <0){
+            this.sideA = 0;
+            System.out.println("Side A can't be negative.");
+        }else{
+            this.sideA = sideA;
+        }
+
+    }
+
+    public int getSideB() {
+        return sideB;
+    }
+
+    public void setSideB(int sideB) {
+        if(sideB <0){
+            this.sideB = 0;
+            System.out.println("Side B can't be negative.");
+        }else {
+            this.sideB = sideB;
+        }
+
+    }
+    public int perimeter(){
+        return (2*sideA) + (2*sideB);
+    }
+    public int area(){
+        return sideA*sideB;
+    }
+
+}
